0512c7b1fae1c4b7ca6d9a3bcd5aafd4659c511f
amesh <amesh.f@gmail.com>
2021-03-01 21:51:20 +1100

    Initial commit

---
+++ lib/first_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(MediaQuery.of(context).size.width, 100),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Text("Hello World", style: TextStyle(fontSize: 20),),
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  child: Text("Name: "),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: CupertinoTextField(
+                ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
